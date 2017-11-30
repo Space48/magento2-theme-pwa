aedf6280371143ebb8ef7534af7c6cf3beb6e3cb
Darren Belding <darren.belding@meanbee.com>
2017-11-30 14:34:48 +0000

    Create add to cart flyout

--- src/Magento_Catalog/web/js/catalog-add-to-cart-mixin.js
+++ src/Magento_Catalog/web/js/catalog-add-to-cart-mixin.js
@@ -1,6 +1,7 @@
 define([
-    'jquery'
-], function ( $ ) {
+    'jquery',
+    'mage/translate',
+], function ( $, $t ) {
     'use strict';
 
     return function (widget) {
@@ -15,6 +16,31 @@ define([
                     e.stopPropagation();
                     self.submitForm($(this));
                 });
+
+                this.options.flyout = '.js-ajaxcart-flyout';
+                this.options.flyoutMessage = '.js-ajaxcart-message';
+            },
+
+            enableAddToCartButton: function(form) {
+                var addToCartButtonTextAdded = this.options.addToCartButtonTextAdded || $t('Added');
+                var self = this,
+                    addToCartButton = $(form).find(this.options.addToCartButtonSelector);
+    
+                addToCartButton.find('span').text(addToCartButtonTextAdded);
+                addToCartButton.attr('title', addToCartButtonTextAdded);
+    
+                setTimeout(function() {
+                    var addToCartButtonTextDefault = self.options.addToCartButtonTextDefault || $t('Add to Cart');
+                    addToCartButton.removeClass(self.options.addToCartButtonDisabledClass);
+                    addToCartButton.find('span').text(addToCartButtonTextDefault);
+                    addToCartButton.attr('title', addToCartButtonTextDefault);
+                }, 1000);
+
+                var flyout = this.element.find(this.options.flyout);
+                var flyoutMessage = flyout.find(this.options.flyoutMessage);
+
+                flyout.addClass("is-active");
+                flyoutMessage.text($t('Added to cart'));
             }
         });
 
